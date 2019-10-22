e87c6ed99b4a6f545588e4dbe8d6939dfefe1228
Bridger Middleton <bmiddy3@gmail.com>
2019-10-22 16:12:12 -0600

    first draft

--- apps/football_app/urls.py
+++ apps/football_app/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     url(r'^registerpage$', views.registerpage),
     url(r'^login$', views.login),
     url(r'^register$', views.register),
+    url(r'^draftpage$', views.draftpage),
+    url(r'^draftplayer$', views.draftplayer)
 
 ]
--- apps/football_app/views.py
+++ apps/football_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 import bcrypt
+import nflgame
+from .models import User, Player, TWeek
 
 def loginpage(request):
     return render(request, "football_app/loginpage.html")
@@ -14,19 +16,62 @@ def login(request):
         logged_user = user[0]
         if bcrypt.checkpw(request.POST['password'].encode(), logged_user.password.encode()):
             request.session['userid'] = logged_user.id
-            pass
+            return redirect("/draftpage")
+        else:
+            return redirect("/")
     else:
         return redirect("/")
 
 def register(request):
     password = request.POST['password']
     pw_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
-    user = User.objects.create(first_name=request.POST['first_name'],last_name=request.POST['last_name'],email=request.POST['email'],password=pw_hash)
+    user = User.objects.create(first_name=request.POST['first_name'],last_name=request.POST['last_name'],email=request.POST['email'],password=pw_hash,team_name=request.POST['team_name'],W=0,L=0)
     request.session['userid'] = user.id
-    pass
+    return redirect("/draftpage")
 
 def logout(request):
    if 'userid' in request.session:
        del request.session['userid']
    return redirect("/")
 # Create your views here.
+def filterStatus(theplayer):
+    if theplayer.status == "ACT":
+        return True
+    else:
+        return False
+def draftpage(request):
+    if 'userid' in request.session:
+
+        all_players = list(nflgame.players.values())
+        active_players = list(filter(filterStatus, all_players))
+        all_player_classes = Player.objects.all()
+        available_players = []
+        for player in active_players:
+            taken = False
+            for taken_player in all_player_classes:
+                if player.gsis_id == taken_player.gsis_id:
+                    taken = True
+                    break
+            if taken == False:
+                available_players.append(player)
+
+            
+
+        context = {
+            "all_players": available_players,
+        }
+
+        return render(request, "football_app/draftpage.html", context)
+    else:
+        return redirect("/")
+def draftplayer(request):
+    all_players = list(nflgame.players.values())
+    active_players = list(filter(filterStatus, all_players))
+    user = User.objects.get(id=request.session['userid'])
+    for a in active_players:
+        if a.gsis_id == request.POST['player']:
+            thisP = a
+    newPlayer = Player.objects.create(first_name = thisP.first_name, last_name = thisP.last_name, gsis_id =thisP.gsis_id, position = thisP.position, total_points=0, passing_yards=0, rushing_yards=0, receiving_yards=0, passing_tds=0, rushing_tds=0, receiving_tds=0, receptions=0, user=user)
+    print(newPlayer)
+    return redirect("/draftpage")
+
